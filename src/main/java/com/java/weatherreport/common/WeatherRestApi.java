cbded63e361396d8d40fc4a55041406867aa1c9a
siddu <siddu.as@gmail.com>
2020-04-11 14:54:35 +0530

    refactoring code

--- src/main/java/com/java/weatherreport/common/WeatherRestApi.java
+++ src/main/java/com/java/weatherreport/common/WeatherRestApi.java
@@ -1,11 +1,18 @@
 package com.java.weatherreport.common;
 
+import com.java.weatherreport.data.CustomReport;
+import com.java.weatherreport.data.list;
 import lombok.AllArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -14,10 +21,40 @@ public class WeatherRestApi {
     private RestTemplate restTemplate;
 
     public ResponseEntity<String> getWeatherReport(String city) {
-        final String uri = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=d2929e9483efc82c82c32ee7e02d563e";
+        final String uri = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=d2929e9483efc82c82c32ee7e02d563e";
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
         return restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+    }
+
+    public CustomReport getCustomReport(list list) {
+        CustomReport customReport = new CustomReport();
+        double minTemp = tempInCelcius(list.getMain().getTemp_min());
+        double maxTemp = tempInCelcius(list.getMain().getTemp_max());
+        customReport.setMinTemperature(minTemp);
+        customReport.setMaxTemperature(maxTemp);
+        customReport.setLocalDate(getDate(list.getDt()));
+
+        Optional<String> weatherCondition = Optional.ofNullable(list)
+                .map(com.java.weatherreport.data.list::getWeather)
+                .map(weathers -> weathers.get(0).getMain());
+
+        if (weatherCondition.isPresent() && maxTemp > 40 && WeatherCondition.CLEAR.getCondition().equals(weatherCondition.get())) {
+            customReport.setAdviceMessage("Use sunscreen lotion");
+        } else if (weatherCondition.isPresent() && WeatherCondition.CLOUDS.getCondition().equals(weatherCondition.get())) {
+            customReport.setAdviceMessage("Carry umbrella");
+        }
+        return customReport;
+    }
+
+    public double tempInCelcius(String tempF) {
+        return Double.parseDouble(tempF) - Double.parseDouble("273.15");
+    }
+
+    public String getDate(long epoc) {
+        LocalDateTime dateTime = LocalDateTime.ofEpochSecond(epoc, 0, ZoneOffset.UTC);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy", Locale.ENGLISH);
+        return dateTime.format(formatter);
     }
 }
--- src/main/java/com/java/weatherreport/restapi/WeatherForecastRestController.java
+++ src/main/java/com/java/weatherreport/restapi/WeatherForecastRestController.java
@@ -3,7 +3,6 @@ package com.java.weatherreport.restapi;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.java.weatherreport.common.WeatherCondition;
 import com.java.weatherreport.common.WeatherRestApi;
 import com.java.weatherreport.data.CustomReport;
 import com.java.weatherreport.data.WeatherReport;
@@ -13,13 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -30,48 +24,19 @@ public class WeatherForecastRestController {
     @GetMapping("wetherreport")
     List<CustomReport> getWeatherReport(@RequestParam(value = "city", defaultValue = "Bangalore") String cityName) {
         List<CustomReport> customReportList = new ArrayList<>();
+        //Call third party API to get forecast details.
         String report = weatherRestApi.getWeatherReport(cityName).getBody();
-        System.out.println(report);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
         WeatherReport weatherReport = gson.fromJson(report, WeatherReport.class);
         List<list> listList = weatherReport.getList();
 
         for (list list : listList) {
-            customReportList.add(getCustomReport(list));
+            customReportList.add(weatherRestApi.getCustomReport(list));
         }
 
         System.out.println(customReportList);
         return customReportList;
-    }
-
-    private CustomReport getCustomReport(list list) {
-        CustomReport customReport = new CustomReport();
-        double minTemp = tempInCelcius(list.getMain().getTemp_min());
-        double maxTemp = tempInCelcius(list.getMain().getTemp_max());
-        customReport.setMinTemperature(minTemp);
-        customReport.setMaxTemperature(maxTemp);
-        customReport.setLocalDate(getDate(list.getDt()));
-
-        Optional<String> weatherCondition = Optional.ofNullable(list)
-                .map(com.java.weatherreport.data.list::getWeather)
-                .map(weathers -> weathers.get(0).getMain());
-
-        if (weatherCondition.isPresent() && maxTemp > 40 && WeatherCondition.CLEAR.getCondition().equals(weatherCondition.get())) {
-            customReport.setAdviceMessage("Use sunscreen lotion");
-        } else if (weatherCondition.isPresent() && WeatherCondition.CLOUDS.getCondition().equals(weatherCondition.get())) {
-            customReport.setAdviceMessage("Carry umbrella");
-        }
-        return customReport;
-    }
-
-    private double tempInCelcius(String tempF) {
-        return Double.parseDouble(tempF) - Double.parseDouble("273.15");
-    }
-
-    private String getDate(long epoc) {
-        LocalDateTime dateTime = LocalDateTime.ofEpochSecond(epoc, 0, ZoneOffset.UTC);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy", Locale.ENGLISH);
-        return dateTime.format(formatter);
     }
 
 
